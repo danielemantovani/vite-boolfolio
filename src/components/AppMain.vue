6993f2e3ccbf8d58fa5d487f6294aad94caeadfd
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-29 17:11:52 +0200

    feat: create App main & api call

---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+
+import axios from "axios";
+
+export default {
+    data (){
+        return {
+            projects: [],
+        }
+    },
+
+    created(){
+        axios.get('http://127.0.0.1:8000/api/projects').then((resp)=>{
+            console.log(resp);
+        });
+    }
+}
+</script>
+
+<template>
+<h1>ciao mondo </h1>
+</template>
+
+<style scoped>
+
+</style>
